d93ad7dc70f3a86802959ceb9fdb2359b8578a79
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-10 17:58:56 +0500

    created AuditableEntity

---
+++ Photogram/Photogram.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photogram.Domain.Common;
+
+public class AuditableEntity : Entity, IAuditableEntity
+{
+    public DateTimeOffset CreatedDate { get; set; }
+
+    public DateTimeOffset? ModifiedDate { get; set; }
+}
