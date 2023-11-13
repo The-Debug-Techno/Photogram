34b374a9373ef120d403b8cbb271f47a52b0315e
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-11-13 17:18:22 +0500

    Added Like and views models

--- Photogram/Photogram.Domain/Entities/Story.cs
+++ Photogram/Photogram.Domain/Entities/Story.cs
@@ -5,7 +5,14 @@ namespace Photogram.Domain.Entities;
 public class Story : SoftDeletedEntity
 {
     public string ImagePath { get; set; } = default!;
+
     public string Description { get; set; } = default!;
+
     public int DurationInMinutes { get; set; } = 1;
+
     public Guid UserId { get; set; }
+
+    public ICollection<ViewsStory> Stories { get; set; } = new List<ViewsStory>();
+
+    public ICollection<Like> Likes { get; set; } = new List<Like>();
 }
